59f6f83760de6c22b15f47ff419eeefbc956e80b
Dominik Tyc <domikxf@gmail.com>
2020-11-13 17:50:15 +0100

    configure SPA

---
+++ Kongres.Api/Kongres.Api.WebApi/Startup.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.Extensions.Hosting;
+
+namespace Kongres.Api.WebApi
+{
+    public class Startup
+    {
+        private const string reactPath = "../../web-client";
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSpaStaticFiles(config => config.RootPath = $"{reactPath}/build");
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+
+            app.UseRouting();
+
+            // app.UseEndpoints(endpoints => endpoints.MapControllers());
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = reactPath;
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
+            });
+        }
+    }
+}
