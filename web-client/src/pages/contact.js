3752b644130c707b04e7750353e857c47180464f
KamilDonda <dondakamil@gmail.com>
2020-11-15 15:48:20 +0100

    Create pages and configuration routing

---
+++ web-client/src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function Contact() {
+    return (
+      <div>
+          <h1>Contact</h1>
+      </div>  
+    );
+}
+
+export default Contact;
