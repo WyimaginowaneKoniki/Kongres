58d6c780809a240edd60dfff0989c5d57a2c51cf
KamilDonda <dondakamil@gmail.com>
2020-11-19 22:09:37 +0100

    Add AgendaEvents to agenda.js

--- web-client/src/pages/agenda.js
+++ web-client/src/pages/agenda.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import '../App.css';
+import AgendaEvents from '../components/agendaEvent'
 
-function Agenda() {
+function Agenda(props) {
+  const event = {
+    time1: "10:00",
+    time2: "12:00",
+    title: "Opening",
+    content: "Lorem ipsum"
+  };
+
     return (
       <div>
           <h1>Agenda</h1>
+            <AgendaEvents
+              time1={event.time1}
+              time2={event.time2}
+              title={event.title}
+              content={event.content}
+            />
       </div>  
     );
 }
-
 export default Agenda;
