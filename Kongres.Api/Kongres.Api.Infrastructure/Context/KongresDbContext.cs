1271e0d8f7573d1b7531d47b2efae9d2e1762205
Dominik Tyc <domikxf@gmail.com>
2020-11-20 16:53:03 +0100

    Fix formatting in KongresDbContext

--- Kongres.Api/Kongres.Api.Infrastructure/Context/KongresDbContext.cs
+++ Kongres.Api/Kongres.Api.Infrastructure/Context/KongresDbContext.cs
@@ -6,14 +6,12 @@ namespace Kongres.Api.Infrastructure.Context
     public class KongresDbContext : DbContext
     {
         public KongresDbContext(DbContextOptions options) : base(options)
-        {
-            
-        }
+        { }
         public DbSet<Answer> Answers { get; set; }
         public DbSet<Review> Reviews { get; set; }
         public DbSet<ReviewersScienceWork> ReviewersScienceWorks { get; set; }
         public DbSet<ScienceWork> ScienceWorks { get; set; }
         public DbSet<ScienceWorkInfo> ScienceWorkInfos { get; set; }
-        public DbSet<User> Users {get; set;}
+        public DbSet<User> Users { get; set; }
     }
 }
