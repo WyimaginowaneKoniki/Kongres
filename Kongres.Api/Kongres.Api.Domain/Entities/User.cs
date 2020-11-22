82211ca1c4f98190c0bfe14568bdf6994c4395c9
Dominik Tyc <domikxf@gmail.com>
2020-11-22 20:07:18 +0100

    Change column Password to PasswordHash

--- Kongres.Api/Kongres.Api.Domain/Entities/User.cs
+++ Kongres.Api/Kongres.Api.Domain/Entities/User.cs
@@ -6,7 +6,7 @@ namespace Kongres.Api.Domain.Entities
     public class User
     {
         public uint Id { get; set; }
-        public string Password { get; set; }
+        public string PasswordHash { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Email { get; set; }
