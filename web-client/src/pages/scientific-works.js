ab2c3c586de0373620548b69ea856601fa505170
Kwolik <kwolikzsp6@gmail.com>
2020-11-26 17:00:53 +0100

    Setting the scientific-works address

---
+++ web-client/src/pages/scientific-works.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+function ScientificWorks(props) {
+
+    return(
+        <div>
+          <h1>Scientific works</h1>
+        </div>
+    );
+}
+
+export default ScientificWorks;
