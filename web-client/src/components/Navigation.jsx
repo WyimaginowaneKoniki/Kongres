51bca2f9d3178570d48d66e783c50e434335edaa
KamilDonda <dondakamil@gmail.com>
2020-11-27 14:21:40 +0100

    Update Navigation bar (material UI) and delete css from App.css

--- web-client/src/components/Navigation.jsx
+++ web-client/src/components/Navigation.jsx
@@ -1,28 +1,76 @@
 import React from 'react';
 import '../App.css';
 import {Link} from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Logo from '../images/empty-image.png'
 
 function Navigation() {
+    const styles = makeStyles({
+        main: {
+            width: '90%',
+            height: '50px',
+            display: 'block',
+            margin: 'auto',
+            padding: '2%',
+        },
+        logo: {
+            height: '50px',
+            width: '50px',
+            float: 'left',
+        },
+        box: {
+            width: '60%',
+            float: 'right',
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "flex-end",
+            height: '100%',
+            '& a': {
+                textDecoration: 'none',
+                color: 'black',
+            },
+            '& a:hover': {
+                color: 'gray',
+            },
+          },
+    });
+
+    const style = styles();
+
     return (
-        <nav>
-            <ul className = "nav-links">
-            <Link to = '/'>
-                <li>Home</li>
-            </Link>
-            <Link to = '/agenda'>
-                <li>Agenda</li>
-            </Link>
-            <Link to = '/speakers'>
-                <li>Keynote Speakers</li>
-            </Link>
-            <Link to = '/about'>
-                <li>Info</li>
-            </Link>
-            <Link to = '/contact'>
-                <li>Contact</li>
-            </Link>
-            </ul>
-        </nav>
+        <div className={style.main}>
+            <div>
+                <img className={style.logo} src={Logo} alt="Logo"/>
+            </div>
+            <Box className={style.box}>
+                <Box>
+                    <Link to = '/'>
+                        Home
+                    </Link>
+                </Box>
+                <Box>
+                    <Link to = '/agenda'>
+                        Agenda
+                    </Link>
+                </Box>
+                <Box>
+                    <Link to = '/speakers'>
+                        Keynote Speakers
+                    </Link>
+                </Box>
+                <Box>
+                    <Link to = '/about'>
+                        About
+                    </Link>
+                </Box>
+                <Box>
+                    <Link to = '/contact'>
+                        Contact
+                    </Link>
+                </Box>
+            </Box>
+        </div>
     );
 }
 
