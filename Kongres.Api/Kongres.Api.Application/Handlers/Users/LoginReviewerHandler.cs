c787900c2429e4aa4b3e494f2cec2cb14cb96ea7
Dominik Tyc <domikxf@gmail.com>
2020-11-27 19:17:52 +0100

    Add logging

---
+++ Kongres.Api/Kongres.Api.Application/Handlers/Users/LoginReviewerHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Kongres.Api.Application.Commands.Users;
+using Kongres.Api.Domain.Entities;
+using Kongres.Api.Domain.Enums;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kongres.Api.Application.Handlers.Users
+{
+    public class LoginReviewerHandler : AsyncRequestHandler<LoginReviewerCommand>
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public LoginReviewerHandler(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        protected override async Task Handle(LoginReviewerCommand request, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            var userName = $"{UserType.Reviewer}:{request.UserName}";
+
+            var user = await _userManager.FindByNameAsync(userName);
+
+            if (user is null)
+            {
+                throw new Exception("Invalid credentials");
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(user, request.Password, false, false);
+            if (result.Succeeded)
+            {
+                // return login/JWT token
+            }
+        }
+    }
+}
--- Kongres.Api/Kongres.Api.WebApi/Controller/UserController.cs
+++ Kongres.Api/Kongres.Api.WebApi/Controller/UserController.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Kongres.Api.Application.Commands.Users;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -10,8 +11,43 @@ namespace Kongres.Api.WebApi.Controller
 
         public UserController(IMediator mediator) : base(mediator) { }
 
+        // api/User/Register
+        [HttpPost("Register")]
         public async Task<IActionResult> Register([FromBody] CreateUserCommand command)
         {
+            await CommandAsync(command);
+            return Ok();
+        }
+
+        // api/User/LoginParticipant
+        [HttpPost("LoginParticipant")]
+        public async Task<IActionResult> LoginParticipant([FromBody] LoginParticipantCommand command)
+        {
+            try
+            {
+                await CommandAsync(command);
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.Message);
+            }
+
+            return Ok();
+        }
+
+        // api/User/LoginReviewer
+        [HttpPost("LoginReviewer")]
+        public async Task<IActionResult> LoginReviewer([FromBody] LoginReviewerCommand command)
+        {
+            try
+            {
+                await CommandAsync(command);
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.Message);
+            }
+
             await CommandAsync(command);
             return Ok();
         }
