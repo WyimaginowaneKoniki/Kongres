74cd27b13fbbe1a387ac398ca644c8565204bbe5
Dominik Tyc <domikxf@gmail.com>
2020-11-28 13:09:06 +0100

    Connect Review with ScienceWorkFile

--- Kongres.Api/Kongres.Api.Domain/Entities/Review.cs
+++ Kongres.Api/Kongres.Api.Domain/Entities/Review.cs
@@ -5,7 +5,7 @@ namespace Kongres.Api.Domain.Entities
     public class Review
     {
         public uint Id { get; set; }
-        public ReviewersScienceWork ReviewersScienceWork { get; set; }
+        public ScienceWorkFile VersionOfScienceWork { get; set; }
         public string Comment { get; set; }
         public string File { get; set; }
         public DateTime DateReview { get; set; }
