f45cef50a4e44774e3dda275dffe50a4fa006559
KamilDonda <dondakamil@gmail.com>
2020-11-29 19:37:19 +0100

    Add mapping to Scientific-works

--- web-client/src/pages/scientific-works.js
+++ web-client/src/pages/scientific-works.js
@@ -38,143 +38,96 @@ function ScientificWorks(props) {
 
     const style = styles();
 
-    const work1 = {
-        title: 'Importance of Golden Ratio in Mathematics',
-        categories: 'Mathematics',
-        data: '30/11/2020',
-        authors: 'John Doe, Sam Smith, Ashley Blue',
-        text: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place. ',
-    };
+    const works = [
+        {
+            title: 'Importance of Golden Ratio in Mathematics',
+            categories: 'Mathematics',
+            data: '30/11/2020',
+            authors: 'John Doe, Sam Smith, Ashley Blue',
+            text: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place. ',
+        },
+        {
+            title: 'Importance of Golden Ratio in Mathematics',
+            categories: 'Mathematics',
+            data: '30/11/2020',
+            authors: 'John Doe, Sam Smith, Ashley Blue',
+            text: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place. ',
+        },
+        {
+            title: 'Importance of Golden Ratio in Mathematics',
+            categories: 'Mathematics',
+            data: '30/11/2020',
+            authors: 'John Doe, Sam Smith, Ashley Blue',
+            text: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place. ',
+        }
+    ];
 
-    const work2 = {
-        title: 'Importance of Golden Ratio in Mathematics',
-        categories: 'Mathematics',
-        data: '30/11/2020',
-        authors: 'John Doe, Sam Smith, Ashley Blue',
-        text: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place. ',
-    };
+    const workList = works.map(work =>
+        <ScientificWorksOneWork
+            title = {work.title}
+            categories = {work.categories}
+            data = {work.data}
+            authors = {work.authors}
+            text = {work.text}
+            link = {work.link}
+        />
+    )
 
-    const work3 = {
-        title: 'Importance of Golden Ratio in Mathematics',
-        categories: 'Mathematics',
-        data: '30/11/2020',
-        authors: 'John Doe, Sam Smith, Ashley Blue',
-        text: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place. ',
-    };
+    const categories = ['Mathematics', 'Technology', 'Computer Science'];
+    const categoryList = categories.map(name => <ScientificWorksCategories name = {name}/>)
 
-    const category1 = {
-        name: 'Mathematics',
-    }
-
-    const category2 = {
-        name: 'Technology',
-    }
-
-    const category3 = {
-        name: 'Computer Science',
-    }
-
-    const recent1 = {
-        path: picture,
-        alternativeText: 'Photo John Doe',
-        name: 'John Doe',
-        description: 'Academic title or Work title...',
-    }
-
-    const recent2 = {
-        path: picture,
-        alternativeText: 'Photo John Doe',
-        name: 'John Doe',
-        description: 'Title',
-    }
-
-    const recent3 = {
-        path: picture,
-        alternativeText: 'Photo John Doe',
-        name: 'John Doe',
-        description: 'Title',
-    }
-
-    const recent4 = {
-        path: picture,
-        alternativeText: 'Photo John Doe',
-        name: 'John Doe',
-        description: 'Title',
-    }
+    const recents = [
+        {
+            path: picture,
+            alternativeText: 'Photo John Doe',
+            name: 'John Doe',
+            description: 'Academic title or Work title...',
+        },
+        {
+            path: picture,
+            alternativeText: 'Photo John Doe',
+            name: 'John Doe',
+            description: 'Title',
+        },
+        {
+            path: picture,
+            alternativeText: 'Photo John Doe',
+            name: 'John Doe',
+            description: 'Title',
+        },
+        {
+            path: picture,
+            alternativeText: 'Photo John Doe',
+            name: 'John Doe',
+            description: 'Title',
+        }
+    ];
+    
+    const recentList = recents.map(recent =>
+        <ScientificWorksRecentAuthors
+            path = {recent.path}
+            alternativeText = {recent.alternativeText}
+            name = {recent.name}
+            description = {recent.description}
+        />
+    )
 
     return(
         <div className={style.main}>
           <h1>Scientific works</h1>
 
           <div className={style.left}>
-            <ScientificWorksOneWork
-                title = {work1.title}
-                categories = {work1.categories}
-                data = {work1.data}
-                authors = {work1.authors}
-                text = {work1.text}
-                link = {work1.link}
-            />
-            <ScientificWorksOneWork
-                title = {work2.title}
-                categories = {work2.categories}
-                data = {work2.data}
-                authors = {work2.authors}
-                text = {work2.text}
-                link = {work2.link}
-            />
-            <ScientificWorksOneWork
-                title = {work3.title}
-                categories = {work3.categories}
-                data = {work3.data}
-                authors = {work3.authors}
-                text = {work3.text}
-                link = {work3.link}
-            />
+            {workList}
           </div>
 
           <div className={style.right}>
             <Search/>
 
             <h3 className={style.h3}>Categories</h3>
-            <ScientificWorksCategories
-                name = {category1.name}
-            />
-            <ScientificWorksCategories
-                name = {category2.name}
-            />
-            <ScientificWorksCategories
-                name = {category3.name}
-            />
+            {categoryList}
 
             <h3 className={style.h3}>Recent authors / works</h3>
-            <ScientificWorksRecentAuthors
-                path = {recent1.path}
-                alternativeText = {recent1.alternativeText}
-                name = {recent1.name}
-                description = {recent1.description}
-            />
-
-            <ScientificWorksRecentAuthors
-                path = {recent2.path}
-                alternativeText = {recent2.alternativeText}
-                name = {recent2.name}
-                description = {recent2.description}
-            />
-
-            <ScientificWorksRecentAuthors
-                path = {recent3.path}
-                alternativeText = {recent3.alternativeText}
-                name = {recent3.name}
-                description = {recent3.description}
-            />
-
-            <ScientificWorksRecentAuthors
-                path = {recent4.path}
-                alternativeText = {recent4.alternativeText}
-                name = {recent4.name}
-                description = {recent4.description}
-            />
+            {recentList}
           </div>
         </div>
     );
