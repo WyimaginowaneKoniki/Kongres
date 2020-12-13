b585b4f03b1f1493893700f07e973382735ebe71
san <upsu90@gmail.com>
2020-12-13 23:25:51 +0100

    Validate first and last name

--- web-client/src/components/SignUpForm.jsx
+++ web-client/src/components/SignUpForm.jsx
@@ -9,101 +9,126 @@ import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
+import Container from "@material-ui/core/Container";
+import * as yup from "yup";
+import { appendErrors, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import "../App.css";
 
-function SignUpForm(props) {
-  const styles = makeStyles({
-    main: {
-      padding: "2%",
-    },
-    form: {
-      display: "flex",
-      flexDirection: "column",
-      textAlign: "left",
-      width: "400px",
-      margin: "16px",
-    },
-    textfield: {
-      marginBottom: "32px",
-      width: "300px",
-    },
-    formControl: {
-      marginBottom: "32px",
-      width: "300px",
-    },
-    formControlLabel: {
-      marginBottom: "32px",
-    },
-    btnSignup: {
-      width: "100px",
-    },
-    menuItem: {
-      textAlign: "left",
-    },
-    inputLabel: {
-      backgroundColor: "white",
-      padding: "0px 4px",
-      marginLeft: "-4px",
-    },
+const styles = makeStyles({
+  main: {
+    padding: "2%",
+  },
+  form: {
+    display: "flex",
+    flexDirection: "column",
+    textAlign: "left",
+    width: "400px",
+    margin: "16px",
+  },
+  textField: {
+    marginBottom: "32px",
+    width: "300px",
+  },
+  formControl: {
+    marginBottom: "32px",
+    width: "300px",
+  },
+  formControlLabel: {
+    marginBottom: "32px",
+  },
+  btnSignup: {
+    width: "100px",
+  },
+  menuItem: {
+    textAlign: "left",
+  },
+  inputLabel: {
+    backgroundColor: "white",
+    padding: "0px 4px",
+    marginLeft: "-4px",
+  },
+});
+
+export default function SignUpForm() {
+  // const [values, setValues] = React.useState({
+  //   specialization: "",
+  // });
+  const schema = yup.object().shape({
+    firstName: yup
+      .string()
+      .matches(/^[^0-9]*$/, "First name shouldn't contain numbers")
+      .required("Required field"),
+    lastName: yup
+      .string()
+      .matches(/^[^0-9]*$/, "Last name shouldn't contain numbers")
+      .required("Required field")
+  });
+
+  const { register, handleSubmit, control, errors } = useForm({
+    mode: "onBlur",
+    resolver: yupResolver(schema),
   });
 
   const style = styles();
-  const [values, setValues] = React.useState({
-    specialization: "",
-  });
-
-  function handleChange(event) {
-    setValues((oldValues) => ({
-      ...oldValues,
-      [event.target.name]: event.target.value,
-    }));
-  }
 
   return (
-    <div className={style.main}>
-      <form className={style.form} noValidate>
-        <TextField
-          className={style.textfield}
-          required
-          id="first-name-signup"
-          name="first-name"
-          label="First name"
-          autoComplete="first-name"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="outlined"
-        />
+    <Container component="main">
+      <div className={style.main}>
+        <form
+          className={style.form}
+          noValidate
+          onSubmit={handleSubmit((data) => alert(JSON.stringify(data)))}
+        >
+          <TextField
+            className={style.textField}
+            inputRef={register}
+            required
+            id="first-name-signup"
+            name="firstName"
+            label="First name"
+            autoComplete="first-name"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            variant="outlined"
+            error={!!errors.firstName}
+            helperText={errors?.firstName?.message}
+          />
 
-        <TextField
-          className={style.textfield}
-          required
-          id="last-name-signup"
-          name="last-name"
-          label="Last name"
-          autoComplete="family-name"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="outlined"
-        />
+          <TextField
+            className={style.textField}
+            inputRef={register}
+            required
+            id="last-name-signup"
+            name="lastName"
+            label="Last name"
+            autoComplete="family-name"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            variant="outlined"
+            error={!!errors.lastName}
+            helperText={errors?.lastName?.message}
+          />
 
+          {
+            <TextField
+              className={style.textField}
+              required
+              id="email-signup"
+              name="email"
+              label="Email"
+              placeholder="email@example.com"
+              autoComplete="email"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+            />
+            /*
         <TextField
-          className={style.textfield}
-          required
-          id="email-signup"
-          name="email"
-          label="Email"
-          placeholder="email@example.com"
-          autoComplete="email"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="outlined"
-        />
-
-        <TextField
-          className={style.textfield}
+          className={style.textField}
           required
           id="new-password-signup"
           name="new-password"
@@ -118,7 +143,7 @@ function SignUpForm(props) {
         />
 
         <TextField
-          className={style.textfield}
+          className={style.textField}
           id="university-signup"
           name="university"
           label="University"
@@ -130,7 +155,7 @@ function SignUpForm(props) {
         />
 
         <TextField
-          className={style.textfield}
+          className={style.textField}
           id="academic-title-signup"
           name="academic-title"
           label="Academic title"
@@ -148,7 +173,7 @@ function SignUpForm(props) {
           <Select
             displayEmpty
             value={values.specialization}
-            onChange={handleChange}
+            // onChange={handleChange}
             input={
               <OutlinedInput notched name="specialization" id="specialization-signup" />
             }
@@ -179,21 +204,21 @@ function SignUpForm(props) {
 
         <FormControlLabel
           className={style.formControlLabel}
-          control={<Checkbox required name="acceptance" color="primary" />}
+          control={<Checkbox inputRef={register} required name="acceptance" color="primary" defaultValue={false} />}
           label="I accept the Rules of Scienture Conference and I agree to processing my personal data included in the above form by...*"
-        />
+        /> */
+          }
 
-        <Button
-          className={style.btnSignup}
-          color="primary"
-          type="submit"
-          variant="contained"
-        >
-          Sign up
-        </Button>
-      </form>
-    </div>
+          <Button
+            className={style.btnSignup}
+            color="primary"
+            type="submit"
+            variant="contained"
+          >
+            Sign up
+          </Button>
+        </form>
+      </div>
+    </Container>
   );
 }
-
-export default SignUpForm;
