806eb1e18db3473c555fc17dd93e403799191ec8
san <upsu90@gmail.com>
2020-12-15 00:34:06 +0100

    Add password validation

--- web-client/src/components/SignUpForm.jsx
+++ web-client/src/components/SignUpForm.jsx
@@ -10,9 +10,15 @@ import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
 import Container from "@material-ui/core/Container";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import IconButton from "@material-ui/core/IconButton";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import FormHelperText from "@material-ui/core/FormHelperText";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+
 import "../App.css";
 
 const styles = makeStyles({
@@ -51,31 +57,63 @@ const styles = makeStyles({
 });
 
 export default function SignUpForm() {
-  // const [values, setValues] = React.useState({
-  //   specialization: "",
-  // });
+  const [values, setValues] = React.useState({
+    specialization: "",
+    password: "",
+    showPassword: false,
+  });
+
   const schema = yup.object().shape({
     firstName: yup
       .string()
       .matches(/^[A-Za-z]*$/, "First name should only contain letters")
+      .max(26, "First name should be 26 characters or less")
       .required("Required field"),
     lastName: yup
       .string()
       .matches(/^[A-Za-z]*$/, "Last name should only contain letters")
+      .max(26, "Last name should be 26 characters or less")
       .required("Required field"),
     email: yup
       .string()
       .email("Email should have correct format")
       .required("Required field"),
+    password: yup
+      .string()
+      //https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,255}$/,
+        "At least: 12 characters, 1 uppercase, 1 lowercase, 1 number and 1 special"
+      )
+      .min(12, "Password should be at least 12 characters long")
+      .max(255, "Password should be 255 characters or less")
+      .required("Required field"),
+    university: yup.string().max(255, "University should be 255 character long or less"),
+    academicTitle: yup
+      .string()
+      .max(255, "Academic title should be 255 character long or less"),
+    specialization: yup.string().required("Required field"),
   });
 
   const { register, handleSubmit, control, errors } = useForm({
     defaultValues: {
-      email: '',
+      email: "",
     },
     mode: "onBlur",
     resolver: yupResolver(schema),
   });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
 
   const style = styles();
 
@@ -102,7 +140,6 @@ export default function SignUpForm() {
             error={!!errors.firstName}
             helperText={errors?.firstName?.message}
           />
-
           <TextField
             className={style.textField}
             inputRef={register}
@@ -118,108 +155,145 @@ export default function SignUpForm() {
             error={!!errors.lastName}
             helperText={errors?.lastName?.message}
           />
-
-          {
-            <TextField
-              className={style.textField}
-              inputRef={register}
-              required
-              id="email-signup"
-              name="email"
-              label="Email"
-              type="email"
-              placeholder="email@example.com"
-              autoComplete="email"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              variant="outlined"
-              error={!!errors.email}
-              helperText={errors?.email?.message}
-            />
-            /*
-        <TextField
-          className={style.textField}
-          required
-          id="new-password-signup"
-          name="new-password"
-          label="Password"
-          type="password"
-          helperText="Password must contain..."
-          autoComplete="new-password"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="outlined"
-        />
-
-        <TextField
-          className={style.textField}
-          id="university-signup"
-          name="university"
-          label="University"
-          autoComplete="organization"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="outlined"
-        />
-
-        <TextField
-          className={style.textField}
-          id="academic-title-signup"
-          name="academic-title"
-          label="Academic title"
-          autoComplete="job-title"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="outlined"
-        />
-
-        <FormControl variant="outlined" className={style.formControl} required>
-          <InputLabel shrink htmlFor="specialization-signup" className={style.inputLabel}>
-            Specialization
-          </InputLabel>
-          <Select
-            displayEmpty
-            value={values.specialization}
-            // onChange={handleChange}
-            input={
-              <OutlinedInput notched name="specialization" id="specialization-signup" />
-            }
+          <TextField
+            className={style.textField}
+            inputRef={register}
+            required
+            id="email-signup"
+            name="email"
+            label="Email"
+            type="email"
+            placeholder="email@example.com"
+            autoComplete="email"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            variant="outlined"
+            error={!!errors.email}
+            helperText={errors?.email?.message}
+          />
+          <FormControl
+            className={style.textField}
+            required
+            name="password"
+            variant="outlined"
+            inputRef={register}
+            error={!!errors.password}
           >
-            <MenuItem className={style.MenuItem} value="">
-              Select
-            </MenuItem>
-            <MenuItem className={style.MenuItem} value={"Computer Science"}>
-              Computer Science
-            </MenuItem>
-            <MenuItem className={style.MenuItem} value={"Mathematics"}>
-              Mathematics
-            </MenuItem>
-            <MenuItem className={style.MenuItem} value={"Biology"}>
-              Biology
-            </MenuItem>
-            <MenuItem className={style.MenuItem} value={"Chemistry"}>
-              Chemistry
-            </MenuItem>
-            <MenuItem className={style.MenuItem} value={"Psychics"}>
-              Psychics
-            </MenuItem>
-            <MenuItem className={style.MenuItem} value={"Geography"}>
-              Geography
-            </MenuItem>
-          </Select>
-        </FormControl>
-
-        <FormControlLabel
-          className={style.formControlLabel}
-          control={<Checkbox inputRef={register} required name="acceptance" color="primary" defaultValue={false} />}
-          label="I accept the Rules of Scienture Conference and I agree to processing my personal data included in the above form by...*"
-        /> */
-          }
-
+            <InputLabel shrink className={style.inputLabel}>
+              Password
+            </InputLabel>
+            <OutlinedInput
+              inputRef={register}
+              id="password-signup"
+              name="password"
+              type={values.showPassword ? "text" : "password"}
+              value={values.password}
+              onChange={handleChange("password")}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+            {/*  to do: add helper text */}
+            <FormHelperText id="helper-text-password-signup">
+              {errors?.password?.message}
+            </FormHelperText>
+          </FormControl>
+          <TextField
+            className={style.textField}
+            inputRef={register}
+            id="university-signup"
+            name="university"
+            label="University"
+            autoComplete="organization"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            variant="outlined"
+            error={!!errors.university}
+            helperText={errors?.university?.message}
+          />
+          <TextField
+            className={style.textField}
+            inputRef={register}
+            id="academic-title-signup"
+            name="academicTitle"
+            label="Academic title"
+            autoComplete="job-title"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            variant="outlined"
+            error={!!errors.academicTitle}
+            helperText={errors?.academicTitle?.message}
+          />
+          <FormControl
+            variant="outlined"
+            name="specialization"
+            className={style.formControl}
+            required
+            inputRef={register}
+          >
+            <InputLabel
+              shrink
+              htmlFor="specialization-signup"
+              className={style.inputLabel}
+            >
+              Specialization
+            </InputLabel>
+            <Select
+              displayEmpty
+              value={values.specialization}
+              // onChange={handleChange}
+              input={
+                <OutlinedInput notched name="specialization" id="specialization-signup" />
+              }
+            >
+              <MenuItem className={style.MenuItem} value="">
+                Select
+              </MenuItem>
+              <MenuItem className={style.MenuItem} value={"Computer Science"}>
+                Computer Science
+              </MenuItem>
+              <MenuItem className={style.MenuItem} value={"Mathematics"}>
+                Mathematics
+              </MenuItem>
+              <MenuItem className={style.MenuItem} value={"Biology"}>
+                Biology
+              </MenuItem>
+              <MenuItem className={style.MenuItem} value={"Chemistry"}>
+                Chemistry
+              </MenuItem>
+              <MenuItem className={style.MenuItem} value={"Psychics"}>
+                Psychics
+              </MenuItem>
+              <MenuItem className={style.MenuItem} value={"Geography"}>
+                Geography
+              </MenuItem>
+            </Select>
+          </FormControl>
+          <FormControlLabel
+            className={style.formControlLabel}
+            control={
+              <Checkbox
+                inputRef={register}
+                required
+                name="acceptance"
+                color="primary"
+                defaultValue={false}
+              />
+            }
+            label="I accept the Rules of Scienture Conference and I agree to processing my personal data included in the above form by...*"
+          />{" "}
           <Button
             className={style.btnSignup}
             color="primary"
