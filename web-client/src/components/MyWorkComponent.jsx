8e3c73d64550342b58ab0a9cb4b463b0c8050a5c
Kwolik <kwolikzsp6@gmail.com>
2020-12-17 13:20:06 +0100

    Add one review in Current version

--- web-client/src/components/MyWorkComponent.jsx
+++ web-client/src/components/MyWorkComponent.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import '../App.css';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import Tooltip from '@material-ui/core/Tooltip';
+import Box from '@material-ui/core/Box';
+import Rating from '@material-ui/lab/Rating';
+import { DialogContent } from '@material-ui/core';
 
 function MyWorkComponent(props)
 {
@@ -134,10 +138,148 @@ function MyWorkComponent(props)
             marginLeft: '5%',
             float: 'left',
         },
+        currentVersion:
+        {
+            width: '75%',
+            float: 'left',
+            marginTop: '5%',
+            textAlign: 'left',
+        },
+        review:
+        {
+            width: '100%',
+            float: 'left',
+        },
+        leftReview:
+        {
+            width: '15%',
+            float: 'left',
+        },
+        rightReview:
+        {
+            width: '80%',
+            float: 'right',
+        },
+        reviewVersion:
+        {
+            float: 'left',
+            paddingRight: '5%',
+            fontSize: '20px',
+            fontWeight: 'bold',
+            paddingBottom: '3.2%',
+            paddingTop: '1%',
+        },
+        labelVersion:
+        {
+            paddingLeft: '10%',
+        },
+        data:
+        {
+            float: 'left',
+            color: 'grey',
+            paddingRight: '5%',
+            paddingBottom: '3.4%',
+            paddingTop: '1.3%',
+        },
+        starVersion:
+        {
+            float: 'left',
+            width: '20%',
+            paddingTop: '0.3%',
+            paddingBottom: '2%',
+        },
+        image:
+        {
+            width: '70%',
+            height: 70,
+            borderRadius: '50%',
+            float: 'center',
+            marginLeft: '12%',
+        },
+        me:
+        {
+            width: '100%',
+            float: 'left',
+            textAlign: 'center',
+        },
+        rightDate:
+        {
+            float: 'right',
+            color: 'grey',
+        },
+        textReview:
+        {
+            width: '95%',
+            float: 'left',
+            paddingLeft: '2%',
+            fontSize: '16px',
+            paddingBottom: '2%',
+        },
+        reply:
+        {
+            width: '80%',
+            float: 'left',
+            marginLeft: '20%',
+            marginTop: '5%',
+        },
+        replyToReview:
+        {
+            width: '100%',
+            fontWeight: 'bold',
+            float: 'left',
+            fontSize: '20px',
+            marginLeft: '2%',
+        },
+        leftReply:
+        {
+            width: '18%',
+            float: 'left',
+            marginTop: '2.5%',
+        },
+        rightReply:
+        {
+            width: '80%',
+            float: 'right',
+        },
+        textArea:
+        {
+            resize: 'none',
+            border: '2px solid grey',
+            padding: '1%',
+            lineHeight: '1.5',
+            width: '95%',
+            height: 150,
+            fontSize: '15px',
+        },
+        info:
+        {
+            marginLeft: '5%',
+            fontSize: '14px',
+            float: 'left',
+            color:'grey',
+        },
+        btn2:
+        {
+            textTransform: 'none',
+            marginRight: '7%',
+            float: 'right',
+        },
     })
 
-    style = styles()
+    const style = styles();
 
+    const [value] = React.useState(3);  
+    const [hover] = React.useState(3);
+
+    let d = new Date();
+    let hours = `${d.getHours()}:${d.getMinutes()}`;
+    let date = `${d.getDate()}/${d.getMonth()+1}/${d.getFullYear()}`;
+
+    const labels = {
+        1: 'rejected',
+        2: 'correct',
+        3: 'accepted'
+    };
 
     return(
         <div className={style.main}>
@@ -174,8 +316,58 @@ function MyWorkComponent(props)
 
                 <Button variant='outlined' color="primary" 
                     className={style.btn1}>Download full work</Button>
-                <Button variant='contained' color="primary" onClick={handleClickOpen} 
-                    className={style.btn1}>Add review</Button>
+                <Button variant='contained' color="primary"
+                    className={style.btn1}>Add new version</Button>
+            </div>
+
+            <div className={style.currentVersion}>
+                <p>
+                    <span className={style.reviewVersion}>Current version</span>
+                    <span className={style.data}>{date}</span>
+                    <span className={style.starVersion}>
+                    <Tooltip title={labels[hover !== -1 ? hover : value]} placement='top'>
+                        <Box component="fieldset" borderColor="transparent">
+                            <Rating name="read-only" max={3} value={value} readOnly />
+                        </Box>
+                    </Tooltip>
+                    </span>
+                </p>
+                <div className={style.review}>
+                    <div className={style.leftReview}>
+                        <img src={props.path} className={style.image} alt={props.alternativeText}></img>
+                        <span className={style.me}> Reviewer1 </span>
+                    </div>
+                    <div className={style.rightReview}>
+                        <Tooltip title={labels[hover !== -1 ? hover : value]} placement='top-start'>
+                            <Box component="fieldset" borderColor="transparent">
+                                <Rating name="read-only" max={3} value={value} readOnly />
+                                <span className={style.rightDate}>{date} {hours}</span>
+                            </Box>
+                        </Tooltip>
+                        <span className={style.textReview}>{props.review}</span>
+                        <Button variant='outlined' color="primary" 
+                            className={style.btn}>Download review</Button>
+                    </div>
+                </div>
+                <div className={style.reply}>
+                    <span className={style.replyToReview}>Reply to review</span>
+                    <div className={style.leftReply}>
+                        <img src={props.path} className={style.image} alt={props.alternativeText}></img>
+                        <span className={style.me}> John Doe </span>
+                    </div>
+                    <div className={style.rightReply}>
+                        <DialogContent>
+                            <textarea
+                            className={style.textArea} 
+                            placeholder='Add comment'
+                            maxLength='255'
+                            />
+                        </DialogContent>
+                        <span className={style.info}>Remember: you can't edit or delete this commen</span>
+                        <Button variant='contained' color="primary"
+                            className={style.btn2}>Send</Button>
+                    </div>
+                </div>  
             </div>
         </div>
     )
--- web-client/src/pages/my-work.js
+++ web-client/src/pages/my-work.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../App.css';
 import { makeStyles } from '@material-ui/core/styles';
 import picture from '../images/empty-image.png'
-import WorkForReviewComponent from '../components/WorkForReviewComponent';
+import MyWorkComponent from '../components/MyWorkComponent';
 
 function MyWork(props) {
     const styles = makeStyles ({
@@ -24,7 +24,7 @@ function MyWork(props) {
 
     const style = styles();
 
-    const scientificWork = {
+    const myWork = {
         status: 'Status',
         currentDate: '30/11/2020',
         modificationDate: '15/11/2020',
@@ -37,24 +37,26 @@ function MyWork(props) {
         university: 'Silesian University of Technology',
         authors: 'Sam Smith, Ashley Blue',
         text: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place. ',
+        review: 'Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.'
     };
 
     return(
         <div className={style.main}>
             <p className={style.path}>My profile / My Work</p>
-            <WorkForReviewComponent
-                status = {scientificWork.status}
-                currentDate = {scientificWork.currentDate}
-                modificationDate = {scientificWork.modificationDate}
-                name = {scientificWork.name}
-                title = {scientificWork.title}
-                path = {scientificWork.path}
-                alternativeText = {scientificWork.alternativeText}
-                author = {scientificWork.author}
-                degree = {scientificWork.degree}
-                university = {scientificWork.university}
-                authors = {scientificWork.authors}
-                text = {scientificWork.text}
+            <MyWorkComponent
+                status = {myWork.status}
+                currentDate = {myWork.currentDate}
+                modificationDate = {myWork.modificationDate}
+                name = {myWork.name}
+                title = {myWork.title}
+                path = {myWork.path}
+                alternativeText = {myWork.alternativeText}
+                author = {myWork.author}
+                degree = {myWork.degree}
+                university = {myWork.university}
+                authors = {myWork.authors}
+                text = {myWork.text}
+                review = {myWork.review}
                 />
         </div>
     )
