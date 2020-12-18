a702303e51dd07857aef99eaecc869a7772723be
Kamil Donda <dondakamil@gmail.com>
2020-12-18 17:33:54 +0100

    Fix validation

--- web-client/src/pages/adding-work.js
+++ web-client/src/pages/adding-work.js
@@ -4,17 +4,11 @@ import DropZone from "../components/DropZone";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
-import Checkbox from "@material-ui/core/Checkbox";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
 import OutlinedInput from "@material-ui/core/OutlinedInput";
 import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
-import Container from "@material-ui/core/Container";
-import Visibility from "@material-ui/icons/Visibility";
-import VisibilityOff from "@material-ui/icons/VisibilityOff";
-import IconButton from "@material-ui/core/IconButton";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import FormHelperText from "@material-ui/core/FormHelperText";
 import * as yup from "yup";
@@ -88,21 +82,30 @@ function AddingWork(props) {
     description: 0,
   });
 
+  const maxTitleSize = 127;
+  const maxDescriptionSize = 255;
+
   const [specialization, setSpecialization] = React.useState("");
   const schema = yup.object().shape({
     title: yup
       .string()
       .matches(/^[A-Za-z0-9]*$/, "Title should only contain letters and digits")
-      .max(128, "Title should be 128 characters or less")
+      .max(maxTitleSize, `Title should be ${maxTitleSize} characters or less`)
       .required("Required field"),
     description: yup
       .string()
-      .matches(/^[A-Za-z0-9]*$/, "Description should only contain letters and digits")
-      .max(255, "Description should be 255 characters or less")
+      .matches(
+        /^[A-Za-z0-9]*$/,
+        "Description should only contain letters and digits"
+      )
+      .max(
+        maxDescriptionSize,
+        `Description should be ${maxDescriptionSize} characters or less`
+      )
       .required("Required field"),
   });
 
-  const { addingWork, handleSubmit, errors } = useForm({
+  const { register, handleSubmit, errors } = useForm({
     mode: "onBlur",
     resolver: yupResolver(schema),
   });
@@ -126,60 +129,55 @@ function AddingWork(props) {
             onSubmit={handleSubmit((data) => alert(JSON.stringify(data)))}
           >
             {/* Title */}
-            <FormControl
+            <TextField
               className={style.textField}
+              inputRef={register}
               required
+              id="title-adding-work"
               name="title"
+              label="Title"
+              autoComplete="title"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    {counts.title}/{maxTitleSize}
+                  </InputAdornment>
+                ),
+              }}
+              onChange={handleChange("title")}
               variant="outlined"
               error={!!errors.title}
-            >
-              <InputLabel shrink className={style.inputLabel}>
-                Title
-              </InputLabel>
-              <OutlinedInput
-                inputRef={addingWork}
-                id="title-adding-work"
-                name="title"
-                value={values.title}
-                onChange={handleChange("title")}
-                endAdornment={
-                  <InputAdornment position="end">{counts.title}</InputAdornment>
-                }
-              />
-              <FormHelperText error={true} id="helper-text-title-adding-work">
-                {errors?.title?.message}
-              </FormHelperText>
-            </FormControl>
+              helperText={errors?.title?.message}
+            />
 
-            {/* Description */}
-            <FormControl
+            <TextField
               className={style.textField}
+              inputRef={register}
               required
+              id="description-adding-work"
               name="description"
+              label="Description"
+              autoComplete="description"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    {counts.description}/{maxDescriptionSize}
+                  </InputAdornment>
+                ),
+              }}
+              multiline
+              rows={4}
+              onChange={handleChange("description")}
               variant="outlined"
               error={!!errors.description}
-            >
-              <InputLabel shrink className={style.inputLabel}>
-                Description
-              </InputLabel>
-              <OutlinedInput
-                inputRef={addingWork}
-                id="description-adding-work"
-                name="description"
-                multiline
-                rows={4}
-                onChange={handleChange("description")}
-                endAdornment={
-                  <InputAdornment position="end">{counts.description}</InputAdornment>
-                }
-              />
-              <FormHelperText
-                error={true}
-                id="helper-text-description-adding-work"
-              >
-                {errors?.description?.message}
-              </FormHelperText>
-            </FormControl>
+              helperText={errors?.description?.message}
+            />
 
             {/* Specialization Input - Select*/}
             <FormControl
@@ -204,7 +202,7 @@ function AddingWork(props) {
                 input={
                   <OutlinedInput
                     notched
-                    inputRef={addingWork}
+                    inputRef={register}
                     name="specialization"
                     id="specialization-signup"
                   />
