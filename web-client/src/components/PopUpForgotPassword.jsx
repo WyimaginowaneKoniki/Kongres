8f9f6204568a93212dd98febc1515841d01b3026
Kamil Donda <dondakamil@gmail.com>
2020-12-27 12:13:45 +0100

    Create popup as new component

---
+++ web-client/src/components/PopUpForgotPassword.jsx
@@ -0,0 +1,154 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import Dialog from "@material-ui/core/Dialog";
+
+const styles = makeStyles({
+  main: {
+    display: "inline-block",
+  },
+  content: {
+    padding: "30px 100px",
+  },
+  form: {
+    display: "flex",
+    flexDirection: "column",
+    float: "left",
+    textAlign: "left",
+    maxWidth: "400px",
+    margin: "16px",
+  },
+  textField: {
+    marginBottom: "32px",
+    width: "300px",
+  },
+  send: {
+    width: "100px",
+    textTransform: "none",
+    float: "right",
+    marginLeft: "auto",
+    display: "block",
+  },
+  textButton: {
+    textTransform: "none",
+  },
+  message: {
+    textAlign: "center",
+    marginTop: "30px",
+    marginBottom: "30px",
+  },
+});
+
+const sendSuccessfullyStyle = {
+  display: "block",
+  color: "green",
+};
+const sendUnsuccessfullyStyle = {
+  display: "block",
+  color: "red",
+};
+const defaultStyle = {
+  display: "none",
+};
+
+export default function PopUpForgotPassword(props) {
+  const [values, setValues] = React.useState({
+    email: "",
+  });
+
+  const schema = yup.object().shape({
+    email: yup.string().email("Email should have correct format"),
+    //   .required("Required field"),
+  });
+
+  const [open, setOpen] = React.useState(false);
+
+  const { register, handleSubmit, errors } = useForm({
+    mode: "onBlur",
+    resolver: yupResolver(schema),
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+    SetMessageStyle(defaultStyle);
+    values.email = "";
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const style = styles();
+
+  const [_messageStyle, SetMessageStyle] = useState(defaultStyle);
+
+  const sendSuccessfully = (isSuccessfully) => {
+    if (isSuccessfully) SetMessageStyle(sendSuccessfullyStyle);
+    else SetMessageStyle(sendUnsuccessfullyStyle);
+  };
+
+  return (
+    <div className={style.main}>
+      {/* Forgot password */}
+      <Button
+        className={style.textButton}
+        color="primary"
+        onClick={handleClickOpen}
+      >
+        Forgot password?
+      </Button>
+
+      <Dialog open={open} onClose={handleClose}>
+        <span className={style.message}>You will receive link...</span>
+        <div className={style.content}>
+          <form
+            className={style.form}
+            noValidate
+            // If email has been sent successfully or not
+            onSubmit={handleSubmit((data) => {
+                if(sendSuccessfully(false))
+                    alert(JSON.stringify(data))
+            })}
+          >
+            <TextField
+              className={style.textField}
+              inputRef={register}
+              // required
+              id="forgot-email-signin"
+              name="email"
+              label="Login/Email"
+              type="email"
+              value={values.email}
+              autoComplete="email"
+              onChange={handleChange("email")}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+              error={!!errors.email}
+              helperText={errors?.email?.message}
+            />
+            {/* Button Send */}
+            <Button
+              className={style.send}
+              color="primary"
+              type="submit"
+              variant="contained"
+            >
+              Send
+            </Button>
+          </form>
+        </div>
+        <span className={style.message} style={_messageStyle}>Link has been sent</span>
+      </Dialog>
+    </div>
+  );
+}
--- web-client/src/components/SignInForm.jsx
+++ web-client/src/components/SignInForm.jsx
@@ -9,6 +9,7 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Link } from 'react-router-dom';
 import Dialog from '@material-ui/core/Dialog';
+import PopUpForgotPassword from '../components/PopUpForgotPassword';
 
 const styles = makeStyles({
   main: {
@@ -70,156 +71,147 @@ const incorrectStyle = {
 };
 
 export default function SignInForm(props) {
+  const data = {
+    email: "mail@gmail.com",
+    password: "xd",
+  };
 
-    const data = {
-        email: "mail@gmail.com",
-        password: "xd",
-    };
+  const AreEmailAndPasswordCorrect = () => {
+    if (data.email === values.email && data.password === values.password)
+      return true;
+    return false;
+  };
 
-    const AreEmailAndPasswordCorrect = () => {
-        if(data.email === values.email && data.password === values.password)
-            return true;
-        return false;
-    }
+  const [_messageStyle, SetMessageStyle] = useState(correctStyle);
 
-    const [_messageStyle, SetMessageStyle] = useState(correctStyle);
+  const [values, setValues] = React.useState({
+    email: "",
+    password: "",
+  });
 
-    const [values, setValues] = React.useState({
-      email: "",
-      password: "",
-      forgotEmail: "",
-    });
+  const [open, setOpen] = React.useState(false);
 
-    const [open, setOpen] = React.useState(false);
+  const schema = yup.object().shape({
+    email: yup
+      .string()
+      .email("Email should have correct format")
+      .required("Required field"),
 
-    const schema = yup.object().shape({
-      email: yup
-        .string()
-        .email("Email should have correct format")
-        .required("Required field"),
+    password: yup.string().required("Required field"),
+  });
 
-      password: yup.string().required("Required field"),
-    });
+  const { register, handleSubmit, errors } = useForm({
+    mode: "onBlur",
+    resolver: yupResolver(schema),
+  });
 
-    const { register, handleSubmit, errors } = useForm({
-      mode: "onBlur",
-      resolver: yupResolver(schema),
-    });
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
 
-    const handleChange = (prop) => (event) => {
-      setValues({ ...values, [prop]: event.target.value });
-    };
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+  const style = styles();
 
-    const style = styles();
-
-    return (
-      <Container component="main">
-        <div className={style.main}>
-          <div className={style.columns}>
-            <form
-              className={style.form}
-              noValidate
-              onSubmit={handleSubmit((data) => {
-                if (AreEmailAndPasswordCorrect()) {
-                  SetMessageStyle(correctStyle);
-                  alert(JSON.stringify(data));
-                } else SetMessageStyle(incorrectStyle);
-              })}
+  return (
+    <Container component="main">
+      <div className={style.main}>
+        <div className={style.columns}>
+          <form
+            className={style.form}
+            noValidate
+            onSubmit={handleSubmit((data) => {
+              if (AreEmailAndPasswordCorrect()) {
+                SetMessageStyle(correctStyle);
+                alert(JSON.stringify(data));
+              } else SetMessageStyle(incorrectStyle);
+            })}
+          >
+            {/* Login/Email Input */}
+            <TextField
+              className={style.textField}
+              inputRef={register}
+              required
+              id="email-signin"
+              name="email"
+              label="Login/Email"
+              type="email"
+              value={values.email}
+              autoComplete="email"
+              onChange={handleChange("email")}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+              error={!!errors.email}
+              helperText={errors?.email?.message}
+            />
+            {/* Password Input */}
+            <TextField
+              className={style.textField}
+              inputRef={register}
+              required
+              id="password-signin"
+              name="password"
+              label="Password"
+              type="password"
+              value={values.password}
+              autoComplete="current-password"
+              onChange={handleChange("password")}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+              error={!!errors.password}
+              helperText={errors?.password?.message}
+            />
+            {/* Info about correct password and email */}
+            <FormHelperText
+              error={true}
+              style={_messageStyle}
+              className={style.formHelperText}
             >
-              {/* Login/Email Input */}
-              <TextField
-                className={style.textField}
-                inputRef={register}
-                required
-                id="email-signin"
-                name="email"
-                label="Login/Email"
-                type="email"
-                value={values.email}
-                autoComplete="email"
-                onChange={handleChange("email")}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                variant="outlined"
-                error={!!errors.email}
-                helperText={errors?.email?.message}
-              />
-              {/* Password Input */}
-              <TextField
-                className={style.textField}
-                inputRef={register}
-                required
-                id="password-signin"
-                name="password"
-                label="Password"
-                type="password"
-                value={values.password}
-                autoComplete="current-password"
-                onChange={handleChange("password")}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                variant="outlined"
-                error={!!errors.password}
-                helperText={errors?.password?.message}
-              />
-              {/* Info about correct password and email */}
-              <FormHelperText
-                error={true}
-                style={_messageStyle}
-                className={style.formHelperText}
-              >
-                {"Error: Incorrect password or/and email"}
-              </FormHelperText>
-
-              <div>
-                {/* Forgot password */}
-                <Button
-                  className={style.textButton}
-                  color="primary"
-                  onClick={handleClickOpen}
-                >
-                  Forgot password?
-                </Button>
-                {/* Button Submit */}
-                <Button
-                  className={style.btnSignIn}
-                  color="primary"
-                  type="submit"
-                  variant="contained"
-                >
-                  Sign in
-                </Button>
-              </div>
-            </form>
-          </div>
-
-          {/* Info about signing up */}
-          <div className={style.signUp}>
-            <h2 className={style.heading}>{props.heading}</h2>
-            <p className={style.content}>{props.content}</p>
-            <Link to={props.link} style={{ textDecoration: "none" }}>
+              {"Error: Incorrect password or/and email"}
+            </FormHelperText>
+            <div>
+              {/* Forgot password */}
+              <PopUpForgotPassword />
+              {/* Button Submit */}
               <Button
-                variant="outlined"
+                className={style.btnSignIn}
                 color="primary"
-                className={style.btnSignUp}
+                type="submit"
+                variant="contained"
               >
-                {props.btn}
+                Sign in
               </Button>
-            </Link>
-          </div>
+            </div>
+          </form>
         </div>
 
-        <Dialog className={style.mainDialog} open={open} onClose={handleClose}>
+        {/* Info about signing up */}
+        <div className={style.signUp}>
+          <h2 className={style.heading}>{props.heading}</h2>
+          <p className={style.content}>{props.content}</p>
+          <Link to={props.link} style={{ textDecoration: "none" }}>
+            <Button
+              variant="outlined"
+              color="primary"
+              className={style.btnSignUp}
+            >
+              {props.btn}
+            </Button>
+          </Link>
+        </div>
+      </div>
+
+      {/* <Dialog className={style.mainDialog} open={open} onClose={handleClose}>
           <form
             className={style.form}
             noValidate
@@ -243,7 +235,7 @@ export default function SignInForm(props) {
             //   error={!!errors.email}
             //   helperText={errors?.email?.message}
             />
-            {/* Button Send */}
+            Button Send
             <Button
               className={style.btnSignIn}
               color="primary"
@@ -253,7 +245,7 @@ export default function SignInForm(props) {
               Send
             </Button>
           </form>
-        </Dialog>
-      </Container>
-    );
-  }
+        </Dialog> */}
+    </Container>
+  );
+}
