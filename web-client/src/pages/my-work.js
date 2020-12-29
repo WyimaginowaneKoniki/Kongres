8bf94bf7bd9bbb3e2cd3d614e59efc101b408817
Kwolik <kwolikzsp6@gmail.com>
2020-12-29 21:17:41 +0100

    Create list versions of review

--- web-client/src/pages/my-work.js
+++ web-client/src/pages/my-work.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../App.css';
 import { makeStyles } from '@material-ui/core/styles';
-import picture from '../images/empty-image.png'
+import picture from '../images/empty-image.png';
+import Rating from "@material-ui/lab/Rating";
 import MyWorkComponent from '../components/MyWorkComponent';
+import CurrentVersionWithReplyToReview from '../components/CurrentVersionWithReplyToReview';
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import ArrowDropUpIcon from "@material-ui/icons/ArrowDropUp";
 
 function MyWork(props) {
     const styles = makeStyles ({
@@ -19,6 +23,28 @@ function MyWork(props) {
         title:
         {
             fontWeight: 'bold',
+        },
+        menu: 
+        {
+            float: "left",
+            width: "80%",
+            marginTop: "5%",
+            marginLeft: "10%",
+        },
+        row:
+        {
+            padding: "2%",
+            float: "left",
+            width: "80%",
+        },
+        version: 
+        {
+            float: "left",
+            marginLeft: "8%",
+        },
+        arrow: 
+        {
+            float: "right",
         },
     });
 
@@ -37,8 +63,122 @@ function MyWork(props) {
         university: 'Silesian University of Technology',
         authors: 'Sam Smith, Ashley Blue',
         text: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place. ',
-        review: 'Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.'
     };
+
+    const versions = [
+        {
+            title: "Current Version",
+            date: "30/12/2020",
+            stars: 3,
+            review1: [
+                {
+                    name: 'Reviewer1',
+                    path: picture,
+                    date: "30/12/2020",
+                    stars: 3,
+                    textReview: 'Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.'
+                }
+            ],
+            review2: [
+                {
+                    name: 'Reviewer2',
+                    path: picture,
+                    date: "28/12/2020",
+                    stars: 3,
+                    textReview: 'Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.'
+                }
+            ],
+            review3: [
+                {
+                    name: 'Reviewer3',
+                    path: picture,
+                    date: "22/12/2020",
+                    stars: 3,
+                    textReview: 'Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.'
+                }
+            ]
+        },
+        {
+            title: "Previous Version",
+            date: "10/12/2020",
+            stars: 2,
+            review1: [
+                {
+                    name: 'Reviewer1',
+                    path: picture,
+                    date: "10/12/2020",
+                    stars: 2,
+                    textReview: 'Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.'
+                }
+            ],
+            review2: [
+                {
+                    name: 'Reviewer2',
+                    path: picture,
+                    date: "08/12/2020",
+                    stars: 2,
+                    textReview: 'Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.'
+                }
+            ],
+            review3: [
+                {
+                    name: 'Reviewer3',
+                    path: picture,
+                    date: "07/12/2020",
+                    stars: 2,
+                    textReview: 'Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.'
+                }
+            ]
+        }
+    ]
+
+    const [open, setOpen] = useState([true, ...(new Array(versions.length - 1).fill(false))]);
+
+    function handleOnClick(item) {
+    if (!open[item])
+      open[item] = true;
+    else
+      open[item] = false;
+
+    setOpen([...open]);
+  }
+
+  const reviewList = versions.map((review, i) => (
+    <div>
+      <div className={style.row} onClick={() => handleOnClick(i)}>
+        <span className={style.version}> {review.title} </span>
+        <span className={style.version}> {review.date} </span>
+        <Rating value={review.stars} max={3} className={style.version} readOnly />
+        {open[i] ? 
+          <ArrowDropDownIcon className={style.arrow} />
+        : <ArrowDropUpIcon className={style.arrow} />}
+      </div>
+      {open[i] ? 
+        <CurrentVersionWithReplyToReview
+            name={review.review1[0].name}
+            path={review.review1[0].path}
+            stars={review.review1[0].stars}
+            review={review.review1[0].textReview}
+            date={review.review1[0].date}
+        /> : null}
+        {open[i] ? 
+        <CurrentVersionWithReplyToReview
+            name={review.review2[0].name}
+            path={review.review2[0].path}
+            stars={review.review2[0].stars}
+            review={review.review2[0].textReview}
+            date={review.review2[0].date}
+        /> : null}
+        {open[i] ? 
+        <CurrentVersionWithReplyToReview
+            name={review.review3[0].name}
+            path={review.review3[0].path}
+            stars={review.review3[0].stars}
+            review={review.review3[0].textReview}
+            date={review.review3[0].date}
+        /> : null}
+    </div>
+  ));
 
     return(
         <div className={style.main}>
@@ -55,9 +195,8 @@ function MyWork(props) {
                 degree = {myWork.degree}
                 university = {myWork.university}
                 authors = {myWork.authors}
-                text = {myWork.text}
-                review = {myWork.review}
-                />
+            />
+            <div className={style.menu}>{reviewList}</div>
         </div>
     )
 }
