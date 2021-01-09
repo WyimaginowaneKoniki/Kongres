3f90696b220b59e5119187cd2f392a603dafa172
Dominik Tyc <domikxf@gmail.com>
2021-01-09 19:34:56 +0100

    ScienceWork contains collection of versions

---
+++ Kongres.Api/Kongres.Api.Infrastructure/Migrations/20210109183349_ScienceWork contains collection of versions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kongres.Api.Infrastructure.Migrations
+{
+    public partial class ScienceWorkcontainscollectionofversions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
