30e30807284c2f97cd585427626fbc01ac3f801a
Dominik Tyc <domikxf@gmail.com>
2021-01-11 09:34:24 +0100

    Add additional references between tables

--- Kongres.Api/Kongres.Api.Domain/Entities/Answer.cs
+++ Kongres.Api/Kongres.Api.Domain/Entities/Answer.cs
@@ -6,7 +6,6 @@ namespace Kongres.Api.Domain.Entities
     {
         public uint Id { get; set; }
         public User User { get; set; }
-        public Review Review { get; set; }
         public string Comment { get; set; }
         public string File { get; set; }
         public DateTime AnswerDate { get; set; }
