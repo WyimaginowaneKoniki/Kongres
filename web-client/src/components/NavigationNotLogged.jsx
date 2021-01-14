c41a6bd4086819c26aa063eec8d0a749d27bdaf2
Kwolik <kwolikzsp6@gmail.com>
2021-01-14 19:33:50 +0100

    Appearance and functionality header logged

--- web-client/src/components/NavigationNotLogged.jsx
+++ web-client/src/components/NavigationNotLogged.jsx
@@ -3,33 +3,73 @@ import "../App.css";
 import { NavLink } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
-import Logo from "../images/empty-image.png";
+import Logo from "../images/logo.png";
+import Button from "@material-ui/core/Button";
 
 function NavigationNotLogged(props) {
   const styles = makeStyles({
     main: {
-      width: "90%",
-      height: "50px",
-      display: "block",
-      margin: "auto",
-      padding: "2%",
+      width: "100vw",
+      height: "123px",
     },
     logo: {
-      height: "50px",
-      width: "50px",
       float: "left",
+      marginLeft: "138px",
+      marginTop: "10.5px",
+      width: "210px",
     },
-    box: {
-      width: "80%",
+    boxBottom: {
+      width: "56vw",
+      marginRight: "138px",
+      paddingBottom: "12px",
       float: "right",
       display: "flex",
-      justifyContent: "space-around",
+      justifyContent: "flex-end",
       alignItems: "flex-end",
-      height: "100%",
+      fontWeight: 700,
+    },
+    boxTop: {
+      width: "56vw",
+      marginRight: "138px",
+      float: "right",
+      height: "88px",
+    },
+    elements: {
+      float: "right",
+      marginTop: "34px",
+      marginRight: "40px",
+      fontSize: "14px",
+    },
+    btn1: {
+      float: "right",
+      marginTop: "25px",
+      marginLeft: "32px",
+      width: "90px",
+      height: '35px',
+      textTransform: "none",
+      backgroundColor: '#6069A9',
+      '&:hover':
+      {
+        backgroundColor: '#6069A9C4',
+      }
+    },
+    btn2: {
+      float: "right",
+      marginTop: "25px",
+      width: "90px",
+      height: '35px',
+      textTransform: "none",
+      color: '#54457F',
+      borderColor: '#54457F4D',
+      '&:hover':
+      {
+        borderColor: '#54457F99',
+      }
     },
     link: {
       color: "black",
       textDecoration: "none",
+      marginLeft: "64px",
     },
     activeLink: {
       color: "blue",
@@ -41,9 +81,37 @@ function NavigationNotLogged(props) {
   return (
     <div className={style.main}>
       <Box>
-      <NavLink exact to="/"><img className={style.logo} src={Logo} alt="Logo" /> </NavLink>
-        </Box>
-      <Box className={style.box}>
+        <NavLink exact to="/">
+          <img className={style.logo} src={Logo} alt="Logo" />
+        </NavLink>
+      </Box>
+      <div className={style.boxTop}>
+        <NavLink exact to="/signin-participant">
+          <Button
+            className={style.btn1}
+            color="primary"
+            type="submit"
+            variant="contained"
+          >
+            Sign in
+          </Button>
+        </NavLink>
+        <NavLink exact to="/signup-participant">
+          <Button
+            className={style.btn2}
+            color="primary"
+            type="submit"
+            variant="outlined"
+          >
+            Sign up
+          </Button>
+        </NavLink>
+        <span className={style.elements}>
+          Reviewer? <a href="/signin-reviewer">Sign in</a> or {" "}
+          <a href="/signup-reviewer">Sign up</a>
+        </span>
+      </div>
+      <Box className={style.boxBottom}>
         <Box>
           <NavLink
             exact
