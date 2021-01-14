0e5ddf21c8cc99827195562cfb051653b7c241eb
Kwolik <kwolikzsp6@gmail.com>
2021-01-14 14:20:32 +0100

    Create one elements in MenuItem

--- web-client/src/Constants.js
+++ web-client/src/Constants.js
@@ -1,2 +1,33 @@
 export const URL = "https://localhost:5001";
 export const URL_API = `${URL}/api`;
+
+export const categories = [
+    {
+        value: "Select",
+        label: "Select",
+    },
+    {
+        value: "Computer Science",
+        label: "Computer Science",
+    },
+    {
+        value: "Mathematics",
+        label: "Mathematics",
+    },
+    {
+        value: "Biology",
+        label: "Biology",
+    },
+    {
+        value: "Chemistry",
+        label: "Chemistry",
+    },
+    {
+        value: "Psychics",
+        label: "Psychics",
+    },
+    {
+        value: "Geography",
+        label: "Geography",
+    },
+];
