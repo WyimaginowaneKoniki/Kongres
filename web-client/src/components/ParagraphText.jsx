98e3f24c0665dd35ccf4c6bad7e7fbeac269d36b
san <upsu90@gmail.com>
2021-01-16 18:22:10 +0100

    Remove unnecessary styles from ParagraphText

--- web-client/src/components/ParagraphText.jsx
+++ web-client/src/components/ParagraphText.jsx
@@ -1,33 +1,25 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import '../App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import "../App.css";
 
 function ParagraphText(props) {
-    const styles = makeStyles({
-        main: {
-            padding: '2%',
-        },
-        heading: {
-            textAlign: 'left',
-            margin: '0 20%',
-            width: '60%',
-        },
-        content: {
-            textAlign: 'left',
-            margin: '0 20%',
-            display: 'block',
-            width: '60%',
-        },
-      });
+  const styles = makeStyles({
+    heading: {
+      textAlign: "left",
+    },
+    content: {
+      textAlign: "left",
+    },
+  });
 
-    const style = styles();
+  const style = styles();
 
-    return (
-        <div className={style.main}>
-            <h2 className={style.heading}>{props.heading}</h2>
-            <p className={style.content}>{props.content}</p>
-        </div>
-    )
+  return (
+    <div className={style.main}>
+      <h2 className={style.heading}>{props.heading}</h2>
+      <p className={style.content}>{props.content}</p>
+    </div>
+  );
 }
 
 export default ParagraphText;
