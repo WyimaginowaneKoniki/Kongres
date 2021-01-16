26a2c7a0e63411cbcb20adb381716e990d462caf
Dominik Tyc <domikxf@gmail.com>
2021-01-16 12:46:16 +0100

    Add IsEmailConfirmed, NormalizedEmail columns to User

--- Kongres.Api/Kongres.Api.Domain/Entities/User.cs
+++ Kongres.Api/Kongres.Api.Domain/Entities/User.cs
@@ -9,9 +9,11 @@ namespace Kongres.Api.Domain.Entities
         public string NormalizedUserName { get; set; }
         public string Surname { get; set; }
         public string Email { get; set; }
+        public string NormalizedEmail { get; set; }
         public string Specialization { get; set; }
         public string University { get; set; }
         public string Degree { get; set; }
         public string Photo { get; set; }
+        public bool IsEmailConfirmed { get; set; }
     }
 }
