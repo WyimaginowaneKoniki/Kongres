a46ef77c58ce42f01e3e99f14cd7076af006d78c
san <upsu90@gmail.com>
2021-01-16 21:27:50 +0100

    Contact fixed

--- web-client/src/components/ContactInfo.jsx
+++ web-client/src/components/ContactInfo.jsx
@@ -1,62 +1,45 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import '../App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import "../App.css";
 
 function ContactInfo(props) {
-    const styles = makeStyles({
-        main:
-        {
-            paddingTop: '3%',
-            paddingLeft: '5%',
-            width: '50%',
-            float: 'left',
-        },
-        left:
-        {
-            width: '30%',
-            float: 'left',
-        },
-        right:
-        {
-            float: 'left',
-            marginTop: '2%',
-        },
-        photo:
-        {
-            display: 'block',
-            width: '70%',
-            borderRadius: '50%',
-        },
-        text:
-        {
-            fontWeight: 'normal',
-        },
-        a:
-        {
-            textDecoration: 'none',
-            color: 'black',
+  const styles = makeStyles({
+    main: {
+      display: "flex",
+      justifyContent: "flex-start",
+      alignItems: "center",
+      marginBottom: "40px",
+    },
+    photo: {
+      width: "60px",
+    },
+    info: {
+      marginLeft: "16px",
+    },
+    a: {
+      textDecoration: "none",
+      color: "black",
 
-            '&:hover':
-            {
-                color: 'grey',
-            }
-        }
-    });
+      "&:hover": {
+        color: "grey",
+      },
+    },
+  });
 
-    const style = styles();
-    const showLink = <a href={props.link} className={style.a}><h3 className={style.text}>{props.name}</h3></a>;
-    const showAddress = <h3 className={style.text}>{props.name}</h3>;
+  const style = styles();
+  const showLink = (
+    <a href={props.link} className={style.a}>
+      <h3 className={style.text}>{props.name}</h3>
+    </a>
+  );
+  const showAddress = <h3 className={style.text}>{props.name}</h3>;
 
-        return (
-        <div className={style.main}>
-            <div className={style.left}>
-                <img src={props.path} className={style.photo} alt={props.alternativeText}></img>
-            </div>
-            <div className={style.right}>
-                {props.link ? showLink : showAddress}
-            </div>
-        </div>
-        )
+  return (
+    <div className={style.main}>
+      <img src={props.path} className={style.photo} alt={props.alternativeText}></img>
+      <div className={style.info}>{props.link ? showLink : showAddress}</div>
+    </div>
+  );
 }
 
-export default ContactInfo; 
+export default ContactInfo;
--- web-client/src/components/ContactMap.jsx
+++ web-client/src/components/ContactMap.jsx
@@ -1,33 +1,24 @@
-import React from 'react';
-import '../App.css';
-import { makeStyles } from '@material-ui/core/styles';
-
+import React from "react";
+import "../App.css";
+import { makeStyles } from "@material-ui/core/styles";
 
 function ContactMap(props) {
-    const styles = makeStyles({
-        maps:
-        {
-            padding: '2%',
-            paddingRight: '5%',
-            marginTop: '5%',
-            width: 480,
-        },
-        map:
-        {
-            display: 'block',
-            width: "100%",
-            height: 480,
-        }
+  const styles = makeStyles({
+    map: {
+      width: "100%",
+      marginLeft: "40px",
+    },
+  });
 
-    });
+  const style = styles();
 
-    const style = styles();
-
-    return(
-        <div className={style.maps}>
-            <iframe className={style.map} src={props.path} title="Google Maps address Kaszubska 23"></iframe>
-        </div>
-    )
+  return (
+    <iframe
+      className={style.map}
+      src={props.path}
+      title="Google Maps address Kaszubska 23"
+    ></iframe>
+  );
 }
 
-export default ContactMap; 
+export default ContactMap;
