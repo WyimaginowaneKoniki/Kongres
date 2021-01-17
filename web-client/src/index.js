4d85da555066318705124ba565716e73fec5adfb
san <upsu90@gmail.com>
2021-01-17 17:28:13 +0100

    Add global material UI primary and secondary colors

--- web-client/src/index.js
+++ web-client/src/index.js
@@ -2,10 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#6069A9",
+    },
+    secondary: {
+      main: "#C0C4E2",
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
