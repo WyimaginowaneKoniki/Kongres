3b1304a677f417df7813ee1418c11d2e1af4be49
Kwolik <kwolikzsp6@gmail.com>
2021-01-18 19:58:37 +0100

    Correcting Agenda

--- web-client/src/components/AgendaEvent.jsx
+++ web-client/src/components/AgendaEvent.jsx
@@ -5,21 +5,29 @@ import "../App.css";
 export default function AgendaEvent(props) {
   const style = makeStyles({
     main: {
+      width: '70%',
+      margin: 'auto',
       display: "flex",
       justifyContent: "flexStart",
-      padding: "24px",
       marginBottom: "24px",
+      padding: "1em",
+      borderRadius: "4px",
       backgroundColor: "#DFE2F8",
+      textAlign: "left",
     },
     title: {
-      textAlign: "left",
+      color: "#54457F",
     },
     time: {
-      textAlign: "left",
-      width: "320px",
+      width: "130px",
+      opacity: 0.8,
+      color: "#54457F",
     },
     desc: {
-      textAlign: "left",
+      color: "#59546C",
+    },
+    info: {
+      marginLeft: "40px",
     },
   })();
 
@@ -28,7 +36,7 @@ export default function AgendaEvent(props) {
       <h3 className={style.time}>
         {props.time1} - {props.time2}
       </h3>
-      <div className={style.eventInfo}>
+      <div className={style.info}>
         <h2 className={style.title}>{props.title}</h2>
         <p className={style.desc}>{props.content}</p>
       </div>
