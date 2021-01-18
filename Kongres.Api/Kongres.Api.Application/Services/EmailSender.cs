3c4e41489b6da7e34262de6510aaef394ee1c0eb
Kamil Donda <dondakamil@gmail.com>
2021-01-18 12:49:44 +0100

    Adapt to PR

--- Kongres.Api/Kongres.Api.Application/Services/EmailSender.cs
+++ Kongres.Api/Kongres.Api.Application/Services/EmailSender.cs
@@ -4,18 +4,18 @@ using System.Threading.Tasks;
 
 namespace Kongres.Api.Application.Services
 {
-    public class EmailSenderService : IEmailSender
+    public class EmailSender : IEmailSender
     {
         private readonly IEmailService _emailService;
 
-        public EmailSenderService(IEmailService emailService)
+        public EmailSender(IEmailService emailService)
         {
             _emailService = emailService;
         }
 
         public async Task SendConfirmationEmailAsync(uint userId, string userEmail, string confirmationToken)
         {
-            var link = $"https://localhost:5001/email-confirm-token?confirmToken={confirmationToken}&userId={userId}";
+            var link = $"https://localhost:5001/activation?confirmToken={confirmationToken}&userId={userId}";
 
             var message = $"<a href='{link}'>Please confirm email</a>";
             await _emailService.SendAsync(userEmail, "Verify account", message, true);
