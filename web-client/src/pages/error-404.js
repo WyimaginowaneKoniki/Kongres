769a59890710e1f42ba1c94d847e780fa60a538e
Kwolik <kwolikzsp6@gmail.com>
2021-01-19 15:08:15 +0100

    Create page 404

--- web-client/src/pages/error-404.js
+++ web-client/src/pages/error-404.js
@@ -1,8 +1,30 @@
 import React from "react";
 import "../App.css";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core/";
+import { NavLink } from "react-router-dom";
+import background from "../images/404.png";
 
-function Error404(props) {
-  return <h1>Error 404</h1>;
+export default function Error404() {
+  const style = makeStyles({
+    photo: {
+      width: "90%",
+    },
+    linkButton: {
+      textDecoration: "none",
+      textTransform: "none",
+    },
+  })();
+
+  return (
+    <div>
+      <h1 className={style.title}>Oops! Page not found</h1>
+      <NavLink exact to="/" className={style.linkButton}>
+        <Button color="primary" variant="contained" className={style.linkButton}>
+          Go to home
+        </Button>
+      </NavLink>
+      <div><img className={style.photo} src={background} alt="404"/></div>
+    </div>
+  );
 }
-
-export default Error404;
