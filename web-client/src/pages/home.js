0baa4c1f36b2dd735d7bb545f522447af752ac1c
Kwolik <kwolikzsp6@gmail.com>
2021-01-20 20:21:38 +0100

    Create page home

--- web-client/src/pages/home.js
+++ web-client/src/pages/home.js
@@ -1,27 +1,398 @@
 import React from "react";
 import "../App.css";
+import {
+  Button,
+  Stepper,
+  Step,
+  StepLabel,
+  StepContent,
+  Paper,
+  Typography,
+} from "@material-ui/core/";
+import {
+  Timeline,
+  TimelineItem,
+  TimelineSeparator,
+  TimelineConnector,
+  TimelineContent,
+  TimelineOppositeContent,
+  TimelineDot,
+} from "@material-ui/lab/";
+import FastfoodIcon from "@material-ui/icons/Fastfood";
+import LaptopMacIcon from "@material-ui/icons/LaptopMac";
+import HotelIcon from "@material-ui/icons/Hotel";
+import RepeatIcon from "@material-ui/icons/Repeat";
 import { makeStyles } from "@material-ui/core/styles";
+import { NavLink } from "react-router-dom";
+import { LINKS } from "../Constants";
+import bckg1 from "../images/home-desktop.png";
+import bckg2 from "../images/home-1280.png";
+import bckg3 from "../images/home-mobile.png";
 
 export default function Home() {
   const style = makeStyles({
-    text:
-  { 
-    textAlign: 'left',
-  },
+    main: {
+      display: "flex",
+      flexDirection: "column",
+      backgroundImage: `url(${bckg1})`,
+      "@media (min-width: 600px) and (max-width: 1280px)": {
+        backgroundImage: `url(${bckg2})`,
+      },
+      "@media (max-width: 600px)": {
+        backgroundImage: `url(${bckg3})`,
+      },
+    },
+    title: {
+      display: "flex",
+      marginTop: "264px",
+      fontSize: "48px",
+    },
+    buttons: {
+      display: "flex",
+      marginBottom: "528px",
+      "@media (max-width: 768px)": {
+        flexWrap: "wrap",
+      },
+    },
+    btn: {
+      width: "204px",
+      height: "70px",
+      fontSize: "32px",
+      marginRight: "40px",
+      textTransform: "none",
+      "@media (max-width: 768px)": {
+        marginTop: '32px',
+      },
+    },
+    link: {
+      textDecoration: "none",
+    },
+    text: {
+      fontWeight: "bold",
+      textAlign: "left",
+    },
+    panel: {
+      display: "flex",
+      textAlign: "left",
+      marginBottom: "125px",
+      "@media (max-width: 768px)": {
+        flexWrap: "wrap",
+      },
+    },
+    textPanel: {
+      width: "1125px",
+    },
+    stepperPanel: {
+      width: "1125px",
+    },
+    information: {
+      textAlign: "left",
+      marginBottom: "128px",
+    },
+    paper: {
+      padding: '0.5em',
+    },
+    paperLeft: {
+      padding: '0.5em',
+      textAlign: 'left',
+    },
   })();
 
+  function getSteps() {
+    return ["Select campaign settings", "Create an ad group", "Create an ad"];
+  }
+
+  function getStepContent(step) {
+    switch (step) {
+      case 0:
+        return `For each ad campaign that you create, you can control how much
+                you're willing to spend on clicks and conversions, which networks
+                and geographical locations you want your ads to show on, and more.`;
+      case 1:
+        return "An ad group contains one or more ads which target a shared set of keywords.";
+      case 2:
+        return `Try out different ad text to see what brings in the most customers,
+                and learn how to enhance your ads using features like ad extensions.
+                If you run into any problems with your ads, find out how to tell if
+                they're running and how to resolve approval issues.`;
+      default:
+        return "Unknown step";
+    }
+  }
+
+  function getStepsTwo() {
+    return ["Select campaign settings", "Create an ad group", "Create an ad"];
+  }
+
+  function getStepTwoContent(step) {
+    switch (step) {
+      case 0:
+        return `For each ad campaign that you create, you can control how much
+                you're willing to spend on clicks and conversions, which networks
+                and geographical locations you want your ads to show on, and more.`;
+      case 1:
+        return "An ad group contains one or more ads which target a shared set of keywords.";
+      case 2:
+        return `Try out different ad text to see what brings in the most customers,
+                and learn how to enhance your ads using features like ad extensions.
+                If you run into any problems with your ads, find out how to tell if
+                they're running and how to resolve approval issues.`;
+      default:
+        return "Unknown step";
+    }
+  }
+
+  const [activeStep, setActiveStep] = React.useState(0);
+  const steps = getSteps();
+  const [activeStepTwo, setActiveStepTwo] = React.useState(0);
+  const stepsTwo = getStepsTwo();
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  const handleNextTwo = () => {
+    setActiveStepTwo((prevActiveStepTwo) => prevActiveStepTwo + 1);
+  };
+
+  const handleBackTwo = () => {
+    setActiveStepTwo((prevActiveStepTwo) => prevActiveStepTwo - 1);
+  };
+
+  const handleResetTwo = () => {
+    setActiveStepTwo(0);
+  };
+
   return (
-    <div>
-      <h1>Let's start our future!</h1>
+    <div className={style.main}>
+      <div className={style.title}>
+        <h2>Let&apos;s start our future!</h2>
+      </div>
+
+      <div className={style.buttons}>
+        <NavLink exact to={LINKS.PARTICIPANT_SIGN_UP} className={style.link}>
+          <Button variant="outlined" color="primary" className={style.btn}>
+            Sign up
+          </Button>
+        </NavLink>
+        <NavLink exact to={LINKS.PARTICIPANT_LOGIN} className={style.link}>
+          <Button variant="contained" color="primary" className={style.btn}>
+            Log in
+          </Button>
+        </NavLink>
+      </div>
 
       <div>
-        <p className={style.text}>
+        <p className={style.information}>
           Scienture is an international web conference that brings together
           researchers from a variety of fields: computer science, mathematics,
           biology, chemistry, physics and geography. The main purpose of this
           meeting is to inspire and build knowledge, discuss problems and
           solutions in this area and shape future research directions.
         </p>
+      </div>
+      <div className={style.panel}>
+        <div className={style.textPanel}>
+          <p className={style.text}>Join us as Participant</p>
+          <p>
+            Scienture is an international web conference that brings together
+            researchers from a variety of fields: computer science, mathematics,
+            biology, chemistry, physics and geography. The main purpose of this
+            meeting is to inspire and build knowledge, discuss problems and
+            solutions in this area and shape future research directions.
+          </p>
+        </div>
+        <div className={style.stepperPanel}>
+          <p className={style.text}>What should you do?</p>
+          <Stepper activeStep={activeStep} orientation="vertical">
+            {steps.map((label, index) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+                <StepContent>
+                  <Typography>{getStepContent(index)}</Typography>
+                  <div className={style.actionsContainer}>
+                    <div>
+                      <Button
+                        disabled={activeStep === 0}
+                        variant="outlined"
+                        color="primary"
+                        onClick={handleBack}
+                      >
+                        Back
+                      </Button>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={handleNext}
+                      >
+                        {activeStep === steps.length - 1 ? "Finish" : "Next"}
+                      </Button>
+                    </div>
+                  </div>
+                </StepContent>
+              </Step>
+            ))}
+          </Stepper>
+          {activeStep === steps.length && (
+            <Paper square elevation={0} className={style.resetContainer}>
+              <Typography>
+                All steps completed - you&apos;re finished
+              </Typography>
+              <Button onClick={handleReset} variant="outlined" color="primary">
+                Reset
+              </Button>
+            </Paper>
+          )}
+        </div>
+      </div>
+
+      <div className={style.panel}>
+        <div className={style.stepperPanel}>
+          <p className={style.text}>What should you do?</p>
+          <Stepper activeStep={activeStepTwo} orientation="vertical">
+            {stepsTwo.map((label, index) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+                <StepContent>
+                  <Typography>{getStepTwoContent(index)}</Typography>
+                  <div>
+                    <div>
+                      <Button
+                        disabled={activeStepTwo === 0}
+                        variant="outlined"
+                        color="primary"
+                        onClick={handleBackTwo}
+                      >
+                        Back
+                      </Button>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={handleNextTwo}
+                      >
+                        {activeStepTwo === stepsTwo.length - 1 ? "Finish" : "Next"}
+                      </Button>
+                    </div>
+                  </div>
+                </StepContent>
+              </Step>
+            ))}
+          </Stepper>
+          {activeStepTwo === stepsTwo.length && (
+            <Paper square elevation={0}>
+              <Typography>
+                All steps completed - you&apos;re finished
+              </Typography>
+              <Button onClick={handleResetTwo} variant="outlined" color="primary">
+                Reset
+              </Button>
+            </Paper>
+          )}
+        </div>
+        <div className={style.textPanel}>
+          <p className={style.text}>Join us as Participant</p>
+          <p>
+            Scienture is an international web conference that brings together
+            researchers from a variety of fields: computer science, mathematics,
+            biology, chemistry, physics and geography. The main purpose of this
+            meeting is to inspire and build knowledge, discuss problems and
+            solutions in this area and shape future research directions.
+          </p>
+        </div>
+      </div>
+      <div>
+        <p className={style.information}>
+          Scienture is an international web conference that brings together
+          researchers from a variety of fields: computer science, mathematics,
+          biology, chemistry, physics and geography. The main purpose of this
+          meeting is to inspire and build knowledge, discuss problems and
+          solutions in this area and shape future research directions.
+        </p>
+      </div>
+      <div>
+        <Timeline align="alternate">
+          <TimelineItem>
+            <TimelineOppositeContent>
+              <Typography variant="body2" color="textSecondary">
+                9:30 am
+              </Typography>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot>
+                <FastfoodIcon />
+              </TimelineDot>
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Paper elevation={3} className={style.paperLeft}>
+                <Typography variant="h6" component="h1">
+                  Eat
+                </Typography>
+                <Typography>Because you need strength</Typography>
+              </Paper>
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineOppositeContent>
+              <Typography variant="body2" color="textSecondary">
+                10:00 am
+              </Typography>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot color="primary">
+                <LaptopMacIcon />
+              </TimelineDot>
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Paper elevation={3} className={style.paper}>
+                <Typography variant="h6" component="h1">
+                  Code
+                </Typography>
+                <Typography>Because it&apos;s awesome!</Typography>
+              </Paper>
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineSeparator>
+              <TimelineDot color="primary" variant="outlined">
+                <HotelIcon />
+              </TimelineDot>
+              <TimelineConnector/>
+            </TimelineSeparator>
+            <TimelineContent>
+              <Paper elevation={3} className={style.paperLeft}>
+                <Typography variant="h6" component="h1">
+                  Sleep
+                </Typography>
+                <Typography>Because you need rest</Typography>
+              </Paper>
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineSeparator>
+              <TimelineDot color="secondary">
+                <RepeatIcon />
+              </TimelineDot>
+            </TimelineSeparator>
+            <TimelineContent>
+              <Paper elevation={3} className={style.paper}>
+                <Typography variant="h6" component="h1">
+                  Repeat
+                </Typography>
+                <Typography>Because this is the life you love!</Typography>
+              </Paper>
+            </TimelineContent>
+          </TimelineItem>
+        </Timeline>
       </div>
     </div>
   );
