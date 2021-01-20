9954ae93d39aadbba81164e55eced72b78bdd1bc
Dominik Tyc <domikxf@gmail.com>
2021-01-20 20:42:30 +0100

    Update emails

--- Kongres.Api/Kongres.Api.Application/Services/EmailSender.cs
+++ Kongres.Api/Kongres.Api.Application/Services/EmailSender.cs
@@ -18,66 +18,72 @@ namespace Kongres.Api.Application.Services
         #region Reviewer        
         public async Task SendDoNotGetAssignToAnyWork(string reviewerEmail)
         {
-            const string message = "Unfortunately, you were not assigned to any scientific work, because of too few applicants";
-            await _emailService.SendAsync(reviewerEmail, "Work review", message);
+            var message = "Unfortunately, you were not assigned to any scientific work, because of too few applicants.";
+            message += $"Thank you for your commitment! You can still see scientific works. <a href='{WorkLink}'>Go to scientific works page</a>";
+
+            await _emailService.SendAsync(reviewerEmail, "Bad news", message);
         }
 
         public async Task SendWorkAssignmentInformationAsync(string reviewerEmail, uint workId)
         {
-            var message = $"<a href='{WorkLink}{workId}'>You have been assigned to scientific work! Go check it out</a>";
-            await _emailService.SendAsync(reviewerEmail, "Work review", message, true);
+            var message = $"You have been assigned to scientific work! <a href='{WorkLink}{workId}'>Go check it out</a>";
+            await _emailService.SendAsync(reviewerEmail, "You have been assigned to scientific work", message, true);
         }
 
         public async Task SendReceiveAnswerEmailAsync(string reviewerEmail, uint workId)
         {
-            var message = $"<a href='{WorkLink}{workId}'>You have received a reply to the review!</a>";
-            await _emailService.SendAsync(reviewerEmail, "Work review", message, true);
+            var message = $"You have received a reply to the review! <a href='{WorkLink}{workId}'>Go check it out</a>";
+            await _emailService.SendAsync(reviewerEmail, "Reply to the review", message, true);
         }
 
         public async Task SendAddedNewVersionEmailAsync(string reviewerEmail, uint workId)
         {
-            var message = $"<a href='{WorkLink}{workId}'>A new version of the work has been added. You can review it now!</a>";
-            await _emailService.SendAsync(reviewerEmail, "Work review", message, true);
+            var message = $"A new version of the work has been added. You can review it now! <a href='{WorkLink}{workId}'>Go check it out</a>";
+            await _emailService.SendAsync(reviewerEmail, "New version of work", message, true);
         }
         #endregion
 
         #region AuthorOfWork
         public async Task SendWorkDidNotGetReviewers(string authorEmail)
         {
-            const string message = "Unfortunately, no reviewer has been assigned to your scientific work, because of too few applicants";
-            await _emailService.SendAsync(authorEmail, "Work review", message);
+            var message = "Unfortunately, you were not assigned to any scientific work, because of too few applicants.";
+            message += $"Thank you for your commitment! You can still see scientific works. <a href='{WorkLink}'>Go to scientific works page</a>";
+
+            await _emailService.SendAsync(authorEmail, "Bad news", message);
         }
 
         public async Task SendReviewersAssignmentInformationAsync(string authorEmail, uint workId)
         {
-            var message = $"<a href='{WorkLink}{workId}''>Reviewers have just been assigned to your scientific work! Stay tuned for the reviews</a>";
-            await _emailService.SendAsync(authorEmail, "Work review", message, true);
+            var message = $"Reviewers have just been assigned to your scientific work! Stay tuned for the reviews.";
+            message += $"<a href='{WorkLink}{workId}'>Go to work page</a>";
+            await _emailService.SendAsync(authorEmail, "Stay tuned for the reviews", message, true);
         }
 
         public async Task SendReceiveReviewEmailAsync(string authorEmail, uint workId)
         {
-            var message = $"<a href='{WorkLink}{workId}'>Your work has been reviewed!</a>";
-            await _emailService.SendAsync(authorEmail, "Work review", message, true);
+            var message = $"Your work has been reviewed! <a href='{WorkLink}{workId}'>Go to work page</a>";
+            await _emailService.SendAsync(authorEmail, "New review", message, true);
         }
 
         // When all reviews are received and author can add new version of his work
         public async Task SendNewVersionEnabledEmailAsync(string authorEmail, uint workId)
         {
-            var message = $"<a href='{WorkLink}{workId}'>Your work has been rated by all reviewers. You can add a new version now!</a>";
+            var message = "Your work has been rated by all reviewers and they have some suggestions. You can add a new version now! ";
+            message += $"<a href='{WorkLink}{workId}'>Go to work page</a>";
 
-            await _emailService.SendAsync(authorEmail, "Work review", message, true);
+            await _emailService.SendAsync(authorEmail, "All reviewers added their reviews", message, true);
         }
 
         public async Task SendToAuthorWorkGotAcceptedAsync(string authorEmail, uint workId)
         {
-            var message = $"<a href='{WorkLink}{workId}'>Congratulations! Your scientific work is accepted now</a>";
-            await _emailService.SendAsync(authorEmail, "Work review", message, true);
+            var message = $"Congratulations! Your scientific work has just been accepted! <a href='{WorkLink}{workId}'>See reviews</a>";
+            await _emailService.SendAsync(authorEmail, "Your scientific work has been accepted", message, true);
         }
 
         public async Task SendToAuthorWorkGotRejectedAsync(string authorEmail, uint workId)
         {
-            var message = $"<a href='{WorkLink}{workId}'>Unfortunately, your scientific work is rejected</a>";
-            await _emailService.SendAsync(authorEmail, "Work review", message, true);
+            var message = $"Unfortunately, your scientific work is rejected <a href='{WorkLink}{workId}'>Check reviews</a>";
+            await _emailService.SendAsync(authorEmail, "All reviewers added their reviews", message, true);
         }
         #endregion
 
