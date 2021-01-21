f3edcbff5b8096afc2a949aa035e8a64794a89f0
Kwolik <kwolikzsp6@gmail.com>
2021-01-21 10:08:21 +0100

    change image responsive

--- web-client/src/components/AboutInfo.jsx
+++ web-client/src/components/AboutInfo.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import "../App.css";
 
@@ -24,18 +24,40 @@ export default function AboutInfo(props) {
     },
   })();
 
+  const [left, SetLeft] = React.useState(null);
+  const [right, SetRight] = React.useState(null);
+
   const infos = [
     <h2>{props.head}</h2>,
     <p>{props.text}</p>,
-    <a href={props.link}>{props.adnotation}</a>,
+    <a className={style.link} href={props.link}>
+      {props.adnotation}
+    </a>,
   ];
+
   const image = (
-    <img
-      src={props.path}
-      className={style.photo}
-      alt={props.alternativeText}
-    />
+    <img src={props.path} className={style.photo} alt={props.alternativeText} />
   );
+
+  useEffect(() => {
+    SetLeft(
+      <div className={style.left}>
+        {props.isImageRight || window.innerWidth < 768 ? infos : image}
+      </div>
+    )
+    SetRight(
+      <div className={style.right}>
+        {props.isImageRight || window.innerWidth < 768 ? image : infos}
+      </div>
+    )
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[
+    left,
+    right,
+    props.isImageRight,
+    style.left,
+    style.right,
+  ])
 
   return (
     <div className={style.main}>
