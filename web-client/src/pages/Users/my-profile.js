6e6b1d5aca90bee5a1f9574ee1a232f2f57146e4
Kwolik <kwolikzsp6@gmail.com>
2021-01-21 11:55:42 +0100

    Change page to flex

--- web-client/src/pages/Users/my-profile.js
+++ web-client/src/pages/Users/my-profile.js
@@ -8,36 +8,39 @@ import { NavLink } from "react-router-dom";
 export default function MyProfile(props) {
   const style = makeStyles({
     main: {
-      width: "80%",
-      margin: "auto",
-    },
-    h1: {
-      float: "center",
+      display: "flex",
+      "@media (max-width: 1050px)": {
+        flexWrap: "wrap",
+      },
     },
     left: {
-      float: "left",
       width: "400px",
-      margin: "auto",
-      border: "1px solid black",
+      border: "2px solid black",
     },
     right: {
-      float: "right",
-      width: "50%",
+      marginLeft: "64px",
+      "@media (max-width: 1050px)": {
+        padding: "0.5em",
+        marginTop: '64px',
+        marginLeft: "0",
+      },
     },
     h2: {
       textAlign: "left",
-      marginTop: "10%",
-      marginBottom: "15%",
-      marginLeft: "10%",
+      padding: "1.5em",
       "&:hover": {
         cursor: "pointer",
+        color: "#6069A9",
+      },
+      "@media (max-width: 450px)": {
+        padding: "0em",
       },
     },
     link: {
       color: "black",
       textDecoration: "none",
       "&:active": {
-        color: "blue",
+        color: "#6069A9",
       },
     },
   })();
@@ -53,67 +56,69 @@ export default function MyProfile(props) {
   };
 
   const [panel, SetPanel] = React.useState(true);
-  const [info, SetInfo] = React.useState("blue");
+  const [info, SetInfo] = React.useState("#6069A9");
   const [password, SetPassword] = React.useState("black");
 
   const moveToWork = () => {};
 
   const moveToPersonalInformation = () => {
     SetPanel(true);
-    SetInfo("blue");
+    SetInfo("#6069A9");
     SetPassword("black");
   };
 
   const moveToChangePassword = () => {
     SetPanel(false);
-    SetPassword("blue");
+    SetPassword("#6069A9");
     SetInfo("black");
   };
 
   const moveToLogOut = () => {};
 
   return (
-    <div className={style.main}>
+    <div>
       <h1 className={style.h1}> My Profile</h1>
-      <div className={style.left}>
-        <h2 className={style.h2} onClick={moveToWork}>
-          Add work / My work
-        </h2>
-        <h2
-          className={style.h2}
-          onClick={moveToPersonalInformation}
-          style={{ color: info }}
-        >
-          Personal Information
-        </h2>
-        <h2
-          className={style.h2}
-          onClick={moveToChangePassword}
-          style={{ color: password }}
-        >
-          Change password
-        </h2>
-        <h2 className={style.h2} onClick={moveToLogOut}>
-          Log out
-        </h2>
-        <NavLink exact to="/regulations" className={style.link}>
-          <h2 className={style.h2}>Rules</h2>
-        </NavLink>
-      </div>
-      <div className={style.right}>
-        {panel ? (
-          <PersonalInformation
-            firstName={myProfile.firstName}
-            lastName={myProfile.lastName}
-            email={myProfile.email}
-            academicTitle={myProfile.academicTitle}
-            university={myProfile.university}
-            specialization={myProfile.specialization}
-            participant={myProfile.participant}
-          />
-        ) : (
-          <ChangePassword />
-        )}
+      <div className={style.main}>
+        <div className={style.left}>
+          <h2 className={style.h2} onClick={moveToWork}>
+            Add work / My work
+          </h2>
+          <h2
+            className={style.h2}
+            onClick={moveToPersonalInformation}
+            style={{ color: info }}
+          >
+            Personal Information
+          </h2>
+          <h2
+            className={style.h2}
+            onClick={moveToChangePassword}
+            style={{ color: password }}
+          >
+            Change password
+          </h2>
+          <h2 className={style.h2} onClick={moveToLogOut}>
+            Log out
+          </h2>
+          <NavLink exact to="/regulations" className={style.link}>
+            <h2 className={style.h2}>Rules</h2>
+          </NavLink>
+        </div>
+        <div className={style.right}>
+          {panel ? (
+            <PersonalInformation
+              firstName={myProfile.firstName}
+              lastName={myProfile.lastName}
+              email={myProfile.email}
+              academicTitle={myProfile.academicTitle}
+              university={myProfile.university}
+              specialization={myProfile.specialization}
+              participant={myProfile.participant}
+            />
+          ) : (
+            <ChangePassword />
+          )}
+        </div>
       </div>
     </div>
   );
