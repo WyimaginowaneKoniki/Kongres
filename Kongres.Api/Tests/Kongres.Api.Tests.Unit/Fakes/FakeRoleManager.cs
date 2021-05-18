5d217533b63defe4e1490ba62714b127c40338ac
Dominik Tyc <domikxf@gmail.com>
2021-05-18 22:53:40 +0200

    Add unit test for IdentityDataInitializer

---
+++ Kongres.Api/Tests/Kongres.Api.Tests.Unit/Fakes/FakeRoleManager.cs
@@ -0,0 +1,17 @@
+﻿using Kongres.Api.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+
+namespace Kongres.Api.Tests.Unit.Fakes
+{
+    public class FakeRoleManager : RoleManager<Role>
+    {
+        public FakeRoleManager()
+            : base(new Mock<IRoleStore<Role>>().Object,
+                   null,//new Mock<IEnumerable<IRoleValidator<Role>>>().Object,
+                   null,//new Mock<ILookupNormalizer>().Object,
+                   null,//new Mock<IdentityErrorDescriber>().Object,
+                   null)//new Mock<ILogger<RoleManager<Role>>>().Object)
+        { }
+    }
+}
