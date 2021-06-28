869321c70976cd8135ec818081b58b0cce9a8f4d
Dominik Tyc <domikxf@gmail.com>
2021-06-28 14:38:45 +0200

    Add unit tests for ScientificWorkHelper

---
+++ Kongres.Api/Tests/Kongres.Api.Tests.Unit/Helpers/ScientificWorkHelperTests.cs
@@ -0,0 +1,48 @@
+﻿using Bogus;
+using FluentAssertions;
+using Kongres.Api.Application.Helpers;
+using Kongres.Api.Domain.Entities;
+using System.Linq;
+using Xunit;
+
+namespace Kongres.Api.Tests.Unit.Helpers
+{
+    public class ScientificWorkHelperTests
+    {
+        private readonly Faker _faker = new Faker();
+
+        [Fact]
+        public void GetAuthorsWithOthers()
+        {
+            var mainAuthor = new User()
+            {
+                Name = _faker.Person.FirstName,
+                Surname = _faker.Person.LastName
+            };
+
+            var otherAuthors = string.Join(", ", Enumerable.Range(0, 5).Select(_ => _faker.Person.FullName));
+
+            var expected = $"{UserHelper.GetFullName(mainAuthor)}, {otherAuthors}";
+
+            var returned = ScientificWorkHelper.GetAuthors(mainAuthor, otherAuthors);
+
+            returned.Should().Be(expected);
+        }
+
+        [Fact]
+        public void GetAuthorsWithoutOthers()
+        {
+            var mainAuthor = new User()
+            {
+                Name = _faker.Person.FirstName,
+                Surname = _faker.Person.LastName
+            };
+
+            var expected = _faker.Person.FullName;
+
+            var returned = ScientificWorkHelper.GetAuthors(mainAuthor, null);
+
+            returned.Should().Be(expected);
+        }
+    }
+}
